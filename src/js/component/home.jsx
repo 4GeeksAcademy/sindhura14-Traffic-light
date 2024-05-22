0d5a11dae170b71ac7f117cff85a84036e3d9e5f
sindhura14 <sindhu.kilaru14@gmail.com>
2024-05-22 23:57:15 +0000

    changes for traffic light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,46 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	var [color, setColor] = useState();
+	const [purple, setPurple] = useState(false);
+  
+	function handleClick(e) {
+		setColor(e.target.className)
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	  <>
+		<div className="lightPole"> </div>
+		<div className="trafficLight">
+		  <div
+			className={`red${color === "red" ? " selected" : ""}`}
+			onClick={(e) => setColor("red")}
+		  ></div>
+		  <div
+			className={`yellow${color === "yellow" ? " selected" : ""}`}
+			onClick={(e) => setColor("yellow")}
+		  ></div>
+		  <div
+			className={`green${color === "green" ? " selected" : ""}`}
+			onClick={(e) => setColor("green")}
+		  ></div>
+		  <div
+			className={`purple ${color === "purple" ? "selected" : ""}`}
+			style={purple === false ? { display: "none" } : {}}
+			onClick={(e) => setColor("purple")}
+		  ></div>
 		</div>
+		<button
+			className="btn btn-secondary toggleButton"
+			onClick={() => setPurple(!purple)}
+		  >
+			Toggle purple light
+		  </button>
+	  </>
 	);
 };
 
